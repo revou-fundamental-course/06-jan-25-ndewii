6a08e727ac93f07e3f20c2e93acee053ed97e3d7
nd.ewii <nurlailidwi.ulfah@gmail.com>
2025-01-15 18:02:49 +0800

    update layout+style

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // Ini File javascript
+
+function replaceName() {
+    let user = prompt("Siapa nama Anda?","");
+    document.getElementById("user").innerHTML = user
+}
+
+replaceName ();
+
+function 
